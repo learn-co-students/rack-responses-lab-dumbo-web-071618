7caf9acace26e1d1ad0187c38aee704c4f75f8b1
Gabriel-Batista <gabedbatista@gmail.com>
2018-08-03 17:18:21 -0400

    Done.

---
+++ app/application.rb
@@ -0,0 +1,15 @@
+class Application
+ 
+    def call(env)
+        time = Time.now
+        resp = Rack::Response.new
+
+        if time.hour < 12
+            resp.write "Morning"
+        else
+            resp.write "Afternoon"
+        end
+        resp.finish
+    end
+   
+  end
